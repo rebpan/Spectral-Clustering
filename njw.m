97730bce15492a13c07e7553b9f1638648887255
rebpan <80611555+rebpan@users.noreply.github.com>
2021-06-02 13:44:26 +0800

    On spectral clustering: analysis and an algorithm
    Ng, A., Jordan, M., and Weiss, Y. (2002). On spectral clustering: analysis and an algorithm. In T. Dietterich, S. Becker, and Z. Ghahramani (Eds.), Advances in Neural Information Processing Systems 14 (pp. 849 – 856). MIT Press.

---
+++ njw.m
@@ -0,0 +1,29 @@
+clear;
+close all;
+
+addpath(genpath(pwd));
+
+S = load('.\Datasets\jain.txt');
+true_labels = load('.\Datasets\jain_label.txt');
+k = length(unique(true_labels));
+
+scale_sig = 1;
+tmp = squareform(pdist(S,'squaredeuclidean'))/(2*scale_sig^2);
+A = exp(-tmp);
+
+% figure,imshow(affinity,[]),title('affinity matrix');
+
+D = diag(sum(A,2));
+
+L = (D^-0.5)*A*(D^-0.5);
+
+% select the largest eigenvectors
+[eig_vecs,eig_vals] = eig(L);
+[~,sort_index] = sort(diag(eig_vals),'descend');
+X = eig_vecs(:,sort_index(1:k));
+
+Y = X./repmat(sqrt(sum(X.^2,2)),1,k);
+% figure,plot(Y(:,1),Y(:,2),'ro'),title('Data points after map'),grid on;
+
+result_labels = kmeans(Y,k);
+draw(S,true_labels);
